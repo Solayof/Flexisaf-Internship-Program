88c7d9098e3d70c50516412b3878ed8a10a6a49f
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-03-22 11:01:00 +0100

    update StaffController

--- src/main/java/com/flexisaf/backendinternship/restController/StaffController.java
+++ src/main/java/com/flexisaf/backendinternship/restController/StaffController.java
@@ -1,15 +1,20 @@
 package com.flexisaf.backendinternship.restController;
 
+import com.flexisaf.backendinternship.entity.Teacher;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.*;
 import jakarta.annotation.PostConstruct;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import com.flexisaf.backendinternship.entity.Staff;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -37,35 +42,50 @@ public class StaffController {
                     description = "successfully get staff list"
             )
     })
-    public List<Staff> staff() {
+    public CollectionModel<EntityModel<Staff>> staff() {
+        List<EntityModel<Staff>> stafflist = staff.stream()
+                .map(astaff -> EntityModel.of(astaff,
+                        linkTo(methodOn(StaffController.class).getOne(astaff.getId())).withSelfRel(),
+                        linkTo(methodOn(StaffController.class).staff()).withRel("staff")
+                )).toList();
 
-        return staff;
+        return CollectionModel.of(stafflist,
+                linkTo(methodOn(StaffController.class).staff()).withSelfRel());
     }
 
     @GetMapping("/{id}")
-    public Staff getOne(@PathVariable int id) {
+    public EntityModel<Staff> getOne(@PathVariable int id) {
         if (id == 0 || staff.size()  < id) {
             throw new ErrorResponseException(HttpStatusCode.valueOf(404));
         }
-        return staff.get(id - 1);
+        return EntityModel.of(staff.get(id - 1),
+                linkTo(methodOn(StaffController.class).getOne(id)).withSelfRel(),
+                linkTo(methodOn(StaffController.class).staff()).withRel("staff")
+                );
     }
 
     @PostMapping("")
-    public Staff createOne(@RequestBody Staff newStaff) {
+    public EntityModel<Staff> createOne(@RequestBody Staff newStaff) {
         int id = staff.size();
         newStaff.setId(id);
         staff.add(newStaff);
-        return staff.get(id);
+        return EntityModel.of(staff.get(id),
+                linkTo(methodOn(StaffController.class).createOne(newStaff)).withSelfRel(),
+                linkTo(methodOn(StaffController.class).staff()).withRel("staff")
+        );
     }
 
     @PutMapping("/{id}")
-    public Staff updateOne(@RequestBody Staff newStaff, @PathVariable int id) {
+    public EntityModel<Staff> updateOne(@RequestBody Staff newStaff, @PathVariable int id) {
         if (id == 0 || staff.size()  < id) {
             throw new ErrorResponseException(HttpStatusCode.valueOf(404));
         }
         newStaff.setId(id);
         staff.set(id - 1, newStaff);
-        return staff.get(id - 1);
+        return EntityModel.of(staff.get(id - 1),
+                linkTo(methodOn(StaffController.class).updateOne(newStaff, id)).withSelfRel(),
+                linkTo(methodOn(StaffController.class).staff()).withRel("staff")
+        );
     }
 
 }
