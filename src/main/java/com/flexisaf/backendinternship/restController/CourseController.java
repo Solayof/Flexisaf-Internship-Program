04adc2a1dea03b03f5af929f36b006c3f7a58405
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-03-22 21:02:36 +0100

    create course controller

--- src/main/java/com/flexisaf/backendinternship/restController/CourseController.java
+++ src/main/java/com/flexisaf/backendinternship/restController/CourseController.java
@@ -1,2 +1,14 @@
-package com.flexisaf.backendinternship.restController;public class CourseController {
+package com.flexisaf.backendinternship.restController;
+
+import com.flexisaf.backendinternship.entity.Course;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//@RequestMapping("/courses")
+public class CourseController {
+
+    private Course course;
+
+
 }
