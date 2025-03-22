a4a8f86ff88796f2b72bc115c9d2189b8db71e1f
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-03-22 20:56:53 +0100

    create staff controller

--- 3-springboot-app-with-endpoints/src/main/java/com/flexisaf/backendinternship/restController/StaffController.java
+++ 3-springboot-app-with-endpoints/src/main/java/com/flexisaf/backendinternship/restController/StaffController.java
@@ -1,16 +1,30 @@
 package com.flexisaf.backendinternship.restController;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.annotation.*;
 import jakarta.annotation.PostConstruct;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import com.flexisaf.backendinternship.entity.Staff;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("staff")
 public class StaffController {
     private List<Staff> staff;
+    private final StaffModelAssembler assembler;
+
+    public StaffController(List<Staff> staff, StaffModelAssembler assembler) {
+        this.assembler = assembler;
+        this.staff = staff;
+    }
 
     @PostConstruct
     public void staffList() {
@@ -24,10 +38,46 @@ public class StaffController {
 
     }
 
-    @GetMapping("/staff")
-    public List<Staff> staff() {
+    @GetMapping("")
+    @Operation(summary = "Get list of staff in the server")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "successfully get staff list"
+            )
+    })
+    public CollectionModel<EntityModel<Staff>> staff() {
+        List<EntityModel<Staff>> stafflist = staff.stream()
+                .map(assembler::toModel).toList();
 
-        return staff;
+        return CollectionModel.of(stafflist,
+                linkTo(methodOn(StaffController.class).staff()).withSelfRel());
+    }
+
+    @GetMapping("/{id}")
+    public EntityModel<Staff> getOne(@PathVariable int id) {
+        if (id == 0 || staff.size()  < id) {
+            throw new ErrorResponseException(HttpStatusCode.valueOf(404));
+        }
+        return assembler.toModel(staff.get(id - 1));
+    }
+
+    @PostMapping("")
+    public EntityModel<Staff> createOne(@RequestBody Staff newStaff) {
+        int id = staff.size();
+        newStaff.setId(id);
+        staff.add(newStaff);
+        return assembler.toModel(staff.get(id));
+    }
+
+    @PutMapping("/{id}")
+    public EntityModel<Staff> updateOne(@RequestBody Staff newStaff, @PathVariable int id) {
+        if (id == 0 || staff.size()  < id) {
+            throw new ErrorResponseException(HttpStatusCode.valueOf(404));
+        }
+        newStaff.setId(id);
+        staff.set(id - 1, newStaff);
+        return assembler.toModel(staff.get(id - 1));
     }
 
 }
