32af692af86cb77156ef2630e6c53e055bdaea5d
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-03-22 04:03:31 +0100

    modify rController

--- 3-springboot-app-with-endpoints/src/main/java/com/flexisaf/backendinternship/restController/RootController.java
+++ 3-springboot-app-with-endpoints/src/main/java/com/flexisaf/backendinternship/restController/RootController.java
@@ -9,4 +9,9 @@ public class RootController {
     public String root() {
         return "Spring boot server is running";
     }
+
+   @GetMapping("*")
+    public String NotFound() {
+        return "Not found";
+    }
 }
