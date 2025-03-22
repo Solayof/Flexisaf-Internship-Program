b8ded5b49affa4ed5acbb640d0e9230f7384e58e
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-03-22 05:30:22 +0100

    modify TeacherController

--- src/main/java/com/flexisaf/backendinternship/restController/TeacherController.java
+++ src/main/java/com/flexisaf/backendinternship/restController/TeacherController.java
@@ -2,14 +2,18 @@ package com.flexisaf.backendinternship.restController;
 
 import com.flexisaf.backendinternship.entity.Staff;
 import com.flexisaf.backendinternship.entity.Teacher;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import jakarta.annotation.PostConstruct;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/teachers")
 public class TeacherController {
     private List<Staff> staff;
     private List<Teacher> teachers;
@@ -34,25 +38,25 @@ public class TeacherController {
         teacher.setDiscipline("Mathematics");
         teachers.add(teacher);
 
-        teacher = new Teacher(5);
+        teacher = new Teacher(2);
         teacher.setStaffId(staff.get(0).getId());
         teacher.setFullName(staff.get(0).getFirstName() + " " + staff.get(0).getLastName());
         teacher.setDiscipline("Chemistry");
         teachers.add(teacher);
 
-        teacher = new Teacher(6);
+        teacher = new Teacher(1);
         teacher.setStaffId(staff.get(2).getId());
         teacher.setFullName(staff.get(2).getFirstName() + " " + staff.get(2).getLastName());
         teacher.setDiscipline("Physics");
         teachers.add(teacher);
 
-        teacher = new Teacher(7);
+        teacher = new Teacher(5);
         teacher.setStaffId(staff.get(3).getId());
         teacher.setFullName(staff.get(3).getFirstName() + " " + staff.get(3).getLastName());
         teacher.setDiscipline("Biology");
         teachers.add(teacher);
 
-        teacher = new Teacher(8);
+        teacher = new Teacher(3);
         teacher.setStaffId(staff.get(4).getId());
         teacher.setFullName(staff.get(4).getFirstName() + " " + staff.get(4).getLastName());
         teacher.setDiscipline("Software Engineering");
@@ -60,9 +64,35 @@ public class TeacherController {
 
     }
 
-    @GetMapping("/teachers")
+    @GetMapping("/")
     public List<Teacher> teachers() {
 
         return teachers;
+    }
+
+    @GetMapping("/{id}")
+    public Teacher getOne(@PathVariable int id) {
+        if (teachers.size() <= id) {
+            throw new ErrorResponseException(HttpStatusCode.valueOf(400));
+        }
+        return teachers.get(id);
+    }
+
+    @PostMapping("/")
+    public Teacher createOne(@RequestBody Teacher newTeacher) {
+        int id = teachers.size();
+        newTeacher.setId(id);
+        teachers.add(newTeacher);
+        return teachers.get(id);
+    }
+
+    @PutMapping("/{id}")
+    public Teacher updateOne(@RequestBody Teacher teacher, @PathVariable int id) {
+        if (teachers.size() <= id) {
+            throw new ErrorResponseException(HttpStatusCode.valueOf(400));
+        }
+        teacher.setId(id);
+        teachers.add(teacher);
+        return teachers.get(id);
     }
 }
