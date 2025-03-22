4903fa4b714ba17baab8daf689f21c64212cbb62
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-03-22 20:55:19 +0100

    modified setter and getter of staff entity

--- 3-springboot-app-with-endpoints/src/main/java/com/flexisaf/backendinternship/entity/Staff.java
+++ 3-springboot-app-with-endpoints/src/main/java/com/flexisaf/backendinternship/entity/Staff.java
@@ -1,5 +1,10 @@
 package com.flexisaf.backendinternship.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Staff {
     private int id;
     private String firstName;
@@ -16,34 +21,4 @@ public class Staff {
         this.phone = phone;
     }
 
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPhone() {
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
