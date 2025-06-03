ce416dde6feaa7f050e1a16fc1264e7e10888aac
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-06-04 00:20:54 +0100

    modified teacher entity

--- src/main/java/com/flexisaf/backendinternship/entity/Teacher.java
+++ src/main/java/com/flexisaf/backendinternship/entity/Teacher.java
@@ -1,27 +1,32 @@
 package com.flexisaf.backendinternship.entity;
 
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
+@Table(name = "teachers")
 public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Column(name = "discipline")
     private String discipline;
+    @Column(name = "fullName")
     private String fullName;
-    private int staffId;
+
+    @OneToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name = "staff_id")
+    private Staff staff;
+
     public Teacher() {}
 
     public Teacher(int id) {
         this.id = id;
-    }
-
-    public Teacher(int id, String discipline, String fullName, int staffId) {
-        this.id = id;
-        this.discipline = discipline;
-        this.fullName = fullName;
-        this.staffId = staffId;
     }
 
 }
