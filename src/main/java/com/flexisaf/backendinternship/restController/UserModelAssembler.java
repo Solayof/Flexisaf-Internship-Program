16978d6b66b0ce2fe3ca1908abb6f34095918158
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-06-08 17:34:57 +0100

    update user assembler

--- src/main/java/com/flexisaf/backendinternship/restController/UserModelAssembler.java
+++ src/main/java/com/flexisaf/backendinternship/restController/UserModelAssembler.java
@@ -1,6 +1,6 @@
 package com.flexisaf.backendinternship.restController;
 
-import com.flexisaf.backendinternship.entity.User;
+import com.flexisaf.backendinternship.entity.UserEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
@@ -9,10 +9,10 @@ import org.springframework.stereotype.Component;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @Component
-public class UserModelAssembler implements RepresentationModelAssembler<User, EntityModel<User>> {
+public class UserModelAssembler implements RepresentationModelAssembler<UserEntity, EntityModel<UserEntity>> {
     @SuppressWarnings("null")
     @Override
-    public EntityModel<User> toModel(@NonNull User staff) {
+    public EntityModel<UserEntity> toModel(@NonNull UserEntity staff) {
         return EntityModel.of(staff,
                 linkTo(methodOn(UserController.class).getOne(staff.getId())).withSelfRel(),
                 linkTo(methodOn(UserController.class).users()).withRel("staff")
