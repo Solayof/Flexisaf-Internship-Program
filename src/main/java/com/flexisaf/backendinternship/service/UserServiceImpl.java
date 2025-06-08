2652e7835b9a4f6164b630dc3e1ee2e9a617a3d4
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-06-08 15:39:07 +0100

    create user serice implementation

---
+++ src/main/java/com/flexisaf/backendinternship/service/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.flexisaf.backendinternship.service;
+
+import com.flexisaf.backendinternship.entity.User;
+import com.flexisaf.backendinternship.repository.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return UserdetailsImpl.build(user);
+    }
+
+    public String addUser(User user) {
+        user.setPassword(encoder.encoder(user.getPassword()));
+        userRepository.save(user);
+        return "User added Sucessfully";
+    }
+}
