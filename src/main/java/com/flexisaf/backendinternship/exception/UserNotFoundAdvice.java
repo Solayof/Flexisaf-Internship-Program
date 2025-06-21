92a98694162dc625105f2f04a338a3cd39f108bf
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-06-21 21:19:40 +0100

    nake the user excception handler public

--- src/main/java/com/flexisaf/backendinternship/exception/UserNotFoundAdvice.java
+++ src/main/java/com/flexisaf/backendinternship/exception/UserNotFoundAdvice.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-class UserNotFoundAdvice {
+public class UserNotFoundAdvice {
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String userNotFoundHandler(UserNotFoundException ex) {
