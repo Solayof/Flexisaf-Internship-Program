1cbcf265a041d05761ff3f1821a7cb673950ecff
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-06-21 21:16:53 +0100

    update teacher model

--- src/main/java/com/flexisaf/backendinternship/entity/Teacher.java
+++ src/main/java/com/flexisaf/backendinternship/entity/Teacher.java
@@ -3,12 +3,21 @@ package com.flexisaf.backendinternship.entity;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "teachers")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
