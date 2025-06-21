efdc5c0077240bfe47b9485cfe3a899d6d3d26b5
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-06-21 21:19:07 +0100

    make the teacher exception hancler public

--- src/main/java/com/flexisaf/backendinternship/exception/TeacherNotFoundException.java
+++ src/main/java/com/flexisaf/backendinternship/exception/TeacherNotFoundException.java
@@ -2,8 +2,8 @@ package com.flexisaf.backendinternship.exception;
 
 import java.util.UUID;
 
-class TeacherNotFoundException extends RuntimeException {
-    TeacherNotFoundException(UUID id) {
-        super("Could not find teacher " + id);
+public class TeacherNotFoundException extends RuntimeException {
+    public TeacherNotFoundException(UUID id) {
+        super("Could not find teacher with id: " + id);
     }
 }
