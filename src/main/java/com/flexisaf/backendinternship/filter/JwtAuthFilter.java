d71b89e7bdacef232330dccf6d4c78cd21ec2fb5
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-06-30 15:32:01 +0100

    update

--- src/main/java/com/flexisaf/backendinternship/filter/JwtAuthFilter.java
+++ src/main/java/com/flexisaf/backendinternship/filter/JwtAuthFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,10 +16,13 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flexisaf.backendinternship.exception.InvalidJwtTokenException;
 import com.flexisaf.backendinternship.service.JwtServiceImpl;
 import com.flexisaf.backendinternship.service.UserServiceImpl;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -50,6 +54,14 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-        filterChain.doFilter(request, response);
+
+
+        try {
+            filterChain.doFilter(request, response);
+        } catch (InvalidJwtTokenException e) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.getWriter().write(new ObjectMapper().writeValueAsString(Map.of("error", "Unauthorized", "message", e.getMessage())));
+        }
     }
 }
