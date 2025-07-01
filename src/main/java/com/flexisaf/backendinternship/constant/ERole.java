a635898c986ef92166a94c03618c32926254639a
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-01 04:47:26 +0100

    update roles

--- src/main/java/com/flexisaf/backendinternship/constant/ERole.java
+++ src/main/java/com/flexisaf/backendinternship/constant/ERole.java
@@ -1,8 +1,10 @@
 package com.flexisaf.backendinternship.constant;
 
 public enum ERole {
+    ROLE_SUPERADMIN,
     ROLE_ADMIN,
     ROLE_USER,
-    ROLE_MODERATOR
-    
+    ROLE_MODERATOR,
+    WRITE,
+    READ   
 }
