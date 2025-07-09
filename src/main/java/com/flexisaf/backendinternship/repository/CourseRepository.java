5507541416f4793369ad180d2e048287791c36e5
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-09 16:35:20 +0100

    update course repository

--- src/main/java/com/flexisaf/backendinternship/repository/CourseRepository.java
+++ src/main/java/com/flexisaf/backendinternship/repository/CourseRepository.java
@@ -1,11 +1,13 @@
 package com.flexisaf.backendinternship.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.flexisaf.backendinternship.entity.Course;
+import com.flexisaf.backendinternship.entity.UserEntity;
 
 public interface CourseRepository extends JpaRepository<Course, UUID>{
-    
+   List<Course> findAllByOwner(UserEntity owner); 
 }
